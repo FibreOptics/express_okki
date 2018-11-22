cba9b6214c52f70051409f9d1322e9531c67d47e
FibreOptics <nacalon@hotmail.com>
2018-11-22 15:32:19 +0100

    added jsample

--- server.js
+++ server.js
@@ -10,13 +10,29 @@ app.use(express.static(path.join(__dirname, 'react_okki/public')));
 // An api endpoint that returns a short list of items
 app.get('/api/testitem', (req,res) => {
     var list = ["item1", "item2", "item3"];
+    const jsample = [
+        {
+            id:0,
+            username:"Jonn",
+            email:"jonn@fakemail.true",
+            pass:"alpha"
+        },
+        {
+            id:1,
+            username:"Jann",
+            email:"Jann@fakemail.true",
+            pass:"bravo"
+        },
+    ]
     res.json(list);
+    res.json(jsample);
     console.log('Sent list of items');
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/react_okki/public/index.html'));
+    return res.status(444).send('Nothing Here!');
+    //res.sendFile(path.join(__dirname+'/react_okki/public/index.html'));
 });
 
 const port = process.env.PORT || 5000;
