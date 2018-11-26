9e510649b9789ea32fcd98baf200d8a817b7e50c
FibreOptics <nacalon@hotmail.com>
2018-11-26 01:54:54 +0100

    yarn add cors

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 
+const whitelist = ['http://reactokki.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 app.use(cors());
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'react_okki/public')));
 
 // An api endpoint that returns a short list of items
-app.get('/api/testitem', (req,res) => {
+app.get('/api/testitem',cors(corsOptions), (req,res) => {
     const list = ["item1", "item2", "item3"];
     const jsample = [
         {
@@ -35,7 +46,7 @@ app.get('*', (req,res) =>{
     //res.sendFile(path.join(__dirname+'/react_okki/public/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
