a8546dfe70a6c6605a66d59176cc7e5f436e1fb0
FibreOptics <nacalon@hotmail.com>
2018-11-28 12:19:13 +0100

    retry cors

--- server.js
+++ server.js
@@ -4,33 +4,18 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+  }); */
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'react_okki/public')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/testitem',(req,res) => {
     const list = ["item1", "item2", "item3","cors"];
-    const jsample = [
-        {
-            id:0,
-            username:"Jonn",
-            email:"jonn@fakemail.true",
-            pass:"alpha"
-        },
-        {
-            id:1,
-            username:"Jann",
-            email:"Jann@fakemail.true",
-            pass:"bravo"
-        },
-    ]
     res.json(list);
-    //res.json(jsample);
     console.log('Sent list of items');
 });
 
